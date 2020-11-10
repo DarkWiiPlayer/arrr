99f05380dd133411fff0301e8e1e1de8371d73c3
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2020-11-10 11:31:14 +0100

    Refactor code and restructure internal data

--- arrr/init.lua
+++ arrr/init.lua
@@ -4,7 +4,7 @@
 -- @module arrr
 -- @usage
 -- 	local arrr = require 'arrr'
--- 	parser = arrr { {"Foos a bar", "--foo", "-f", {'bar'}} }
+-- 	parser = arrr { {"Foos a bar", "--foo", "-f", 'bar'} }
 
 --- Parses a list of argument descriptors.
 -- The format is: `{Description, Longhand, Shorthand, parameters, filter}`.
@@ -12,19 +12,15 @@
 local function parse(descriptors)
 	local register = {}
 	for i, descriptor in ipairs(descriptors) do
-		local long, short =
-			descriptor[2] and descriptor[2]:gsub('^%-%-', ''),
-			descriptor[3] and descriptor[3]:gsub('^%-', '')
-
 		local current = {
-			name = long;
+			name = descriptor[2]:gsub("^%-%-", '');
 			description = descriptor[1];
 			params = descriptor[4];
 			filter = descriptor[5]
 		}
 
-		if long then register[long] = current end
-		if short then register[short] = current end
+		if descriptor[2] then register[descriptor[2]] = current end
+		if descriptor[3] then register[descriptor[3]] = current end
 		table.insert(register, current)
 	end
 	return register
@@ -32,7 +28,7 @@ end
 
 --- Handles a command
 -- @treturn Number the position of the first unhandled element in the list
-local function handle_command(data, token, list, start, descriptor, raw)
+local function handle_command(data, token, list, start, descriptor)
 	local result
 	if descriptor then
 		local params = descriptor.params
@@ -72,14 +68,43 @@ local function handle_command(data, token, list, start, descriptor, raw)
 		end
 	else
 		-- Insert as a positional argument so it can maybe be parsed later on
-		table.insert(data, raw)
+		table.insert(data, token)
 	end
 	return start
 end
 
-local __parser = {}
+local __parser = {__index = {}}
+
 function __parser:__call(...)
-	return self:parse(...)
+	return self:evaluate(...)
+end
+
+--- Evaluates a chain of arguments
+function __parser.__index:evaluate(list)
+	local data = {}
+	local index = 1
+	while index <= #list do
+		local current = list[index]
+		index = index + 1
+		if current == '--' then
+			for i = index, #list do
+				table.insert(data, list[i])
+			end
+			return data
+		elseif current:find '^%-%-%a+$' then
+			local descriptor = self[current]
+			index = handle_command(data, current, list, index, descriptor)
+		elseif current:find '^%-%a+$' then
+			for current in current:sub(2):gmatch(".") do
+				current = '-'..current
+				local descriptor = self[current]
+				index = handle_command(data, current, list, index, descriptor)
+			end
+		else
+			table.insert(data, current)
+		end
+	end
+	return data
 end
 
 --- Returns a new commandline parser built from a param list.
@@ -92,34 +117,7 @@ end
 -- 	local data = arrr {'--foo', 'bar', '--bar', 'hello', '-abc', 'baz'}
 local function parser(descriptors)
 	local register = setmetatable(parse(descriptors), __parser)
-
-	function register:parse(list)
-		local data = {}
-		local index = 1
-		while index <= #list do
-			local current = list[index]
-			index = index + 1
-			if current == '--' then
-				for i = index, #list do
-					table.insert(data, list[i])
-				end
-				return data
-			elseif current:find '^%-%-%a+$' then
-				local token = current:sub(3)
-				local descriptor = self[token]
-				index = handle_command(data, token, list, index, descriptor, current)
-			elseif current:find '^%-' then
-				for token in current:sub(2):gmatch(".") do
-					local descriptor = self[token]
-					index = handle_command(data, token, list, index, descriptor, "-"..token)
-				end
-			else
-				table.insert(data, current)
-			end
-		end
-		return data
-	end
-
+	register.evaluate = evaluate
 	return register
 end
 
